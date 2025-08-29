46c78b63c5c0ea7ef984fd4aac569ed3275fc4ac
Alex Brilevski <brilevski.alex@gmail.com>
2025-08-29 20:51:27 +0300

    Game board component was added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Player from './components/Player';
+import GameBoard from './components/GameBoard';
 
 function App() {
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player name="Palyer 1" symbol="X"/>
-          <Player name="Palyer 2" symbol="O"/>
+          <Player name="Palyer 1" symbol="X" />
+          <Player name="Palyer 2" symbol="O" />
         </ol>
-        Game board
+        <GameBoard />
       </div>
       Moves log
     </main>
