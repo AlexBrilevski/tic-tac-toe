673b37415397789102dd9e9b1a40f2cf63312d7b
Alex Brilevski <brilevski.alex@gmail.com>
2025-09-01 20:14:50 +0300

    Logic of defining a winner was added

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,23 @@ function App() {
   const [gameTurns, setGameTurns] = useState([]);
   const gameBoard = initialGameBoard;
   let activePlayer = deriveCurrentPlayer(gameTurns);
+  let winner;
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
     const { row, col } = square;
 
     gameBoard[row][col] = player;
+  }
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
+      winner = firstSquareSymbol;
+    }
   }
 
   function onClickSquare(rowIndex, colIndex) {
@@ -52,6 +63,7 @@ function App() {
           <Player name="Palyer 1" symbol="X" isActive={activePlayer === 'X'} />
           <Player name="Palyer 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
+        {winner && <p>You won, {winner}!</p>}
         <GameBoard board={gameBoard} onClickSquare={onClickSquare} />
       </div>
       <Log turns={gameTurns} />
